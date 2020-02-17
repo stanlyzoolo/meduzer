8bb4499e38fb500f11e156fee4d3b61cbaa67617
Vladimir <stanlyzoolo@gmail.com>
2020-02-17 22:11:42 +0300

    Добавлены теги, поиск по ним и т.д.

--- meduzer/blog/views.py
+++ meduzer/blog/views.py
@@ -4,10 +4,15 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 3)
     page = request.GET.get("page")
     try:
@@ -16,7 +21,9 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, "blog/post/list.html", {"page": page, "posts": posts})
+    return render(
+        request, "blog/post/list.html", {"page": page, "posts": posts, "tag": tag}
+    )
 
 
 def post_detail(request, year, month, day, post):
