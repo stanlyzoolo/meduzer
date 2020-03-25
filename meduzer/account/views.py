221f2174dd6e8ff3835b68e1ee770961343b9bbe
Stanlyzoolo <stanlyzoolo@gmail.com>
2020-03-25 22:27:20 +0300

    Все починено 3

--- meduzer/account/views.py
+++ meduzer/account/views.py
@@ -45,7 +45,7 @@ def register(request: HttpRequest):
             password = user_form.cleaned_data["password1"]
             new_user = authenticate(request, username=username, password=password)
             login(request, new_user)
-            return render(request, "account/register_done.html", {"new_user": new_user})
+            return render(request, "account/dashboard.html", {"new_user": new_user})
     else:
         user_form = UserRegistrationForm()
     return render(request, "account/register.html", {"user_form": user_form})
